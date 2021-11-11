7425936a5373fc03ef1fcad4f9b8c11ffb521c1c
Cristiano Morgante <43195123+CrisMorgantee@users.noreply.github.com>
2021-11-11 16:03:00 -0300

    Update fast syntax zsh

--- install.sh
+++ install.sh
@@ -76,8 +76,8 @@ sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my
 sudo ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme" 
 
 # Fast-syntax-highlight
-git clone https://github.com/zdharma/fast-syntax-highlighting.git \
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
   
 # Auto-suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
